45f0a8c08732f1beeb7aa9673470576522a9d9f8
Mandeep Singh <mandeep.singh8020@gmail.com>
2017-04-06 16:02:52 +0530

    Latest files

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -9,28 +9,87 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.social.oauth1.OAuthToken;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
     private Facebook facebook;
     private ConnectionRepository connectionRepository;
-
+	private final String USER_AGENT = "Mozilla/5.0";
+	private String token;
+	private OAuthToken oauthToken;
+	public String secret;
+	
     public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
         this.connectionRepository = connectionRepository;
     }
 
     @GetMapping
-    public String helloFacebook(Model model) {
+    public @ResponseBody String helloFacebook(Model model) throws Exception {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
             return "redirect:/connect/facebook";
         }
+		String url = "https://graph.facebook.com/v2.8/1223876371042997/members?access_token=270215646759850|2eed635a59b0f5cc07cd87fd354c3761";
 
-        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
-        PagedList<Post> feed = facebook.feedOperations().getFeed();
-        model.addAttribute("feed", feed);
-        return "hello";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+		
+		//try {
+			JSONParser parser = new JSONParser();
+			//Use JSONObject for simple JSON and JSONArray for array of JSON.
+			JSONObject data = (JSONObject) parser.parse(response.toString());//path to the JSON file.
+
+			String json = data.toJSONString();
+			System.out.println(json);
+		/*} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}*/
+		return json;
+		
     }
 
+	
 }
